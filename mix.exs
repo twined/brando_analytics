d7c659402ab36954168e17bd6fc3f0d5bbe62f76
Trond Mjøen <trond@twined.net>
2017-06-11 12:51:42 +0200

    Update for 1.4

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Brando.Analytics.Mixfile do
      compilers: [:gettext, :phoenix] ++ Mix.compilers,
      elixirc_paths: elixirc_paths(Mix.env),
      test_coverage: [tool: ExCoveralls],
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -34,20 +34,20 @@ defmodule Brando.Analytics.Mixfile do
   end
 
   defp deps do
-    [{:phoenix, "~> 1.2"},
+    [{:phoenix, "~> 1.3.0-rc", override: true, optional: true},
      {:gettext, "~> 0.11"},
 
+     {:eightyfour, "~> 0.1"},
+
      # Test dependencies
-     {:ex_machina, "~> 1.0", only: :test},
+     {:ex_machina, "~> 2.0", only: :test},
      {:excoveralls, "~> 0.5", only: :test},
 
      {:brando, github: "twined/brando", branch: "develop", optional: true},
-     {:eightyfour, "~> 0.1"},
 
      # Documentation dependencies
-     {:ex_doc, "~> 0.11", only: :docs},
-     {:inch_ex, "~> 0.5", only: :docs}
-    ]
+     {:ex_doc, "~> 0.12", only: :docs},
+     {:inch_ex, "~> 0.5", only: :docs}]
   end
 
   # Specifies which paths to compile per environment
